25be4407f84b1563ba6ac0a8e39eee06ac094bb8
Unknown <matthewboshears@outlook.com>
2018-04-09 17:38:09 -0400

    Began Modification to new GraphNodes System
    Wrote precursors to unified class system. Needs to be modified from here.

---
+++ Augmented Campus/Augmented Campus/Model/DataImport.swift
@@ -0,0 +1,29 @@
+//
+//  DataImport.swift
+//  Augmented Campus
+//
+//  Created by SWE Capstone on 4/9/18.
+//  Copyright © 2018 Nicholas Setliff. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+func createGraphAndGetDirections(){
+    //In the end, return an array of the graph node.
+    
+
+        for _ in arrayNodes{
+            var counter = 1
+            //this is where we initialize each individual value. Iterate through. I left to take a friend to the doctor here, and you had the idea to collapse all of this into one class - brilliant, in my opinion, but I will need your help on that.
+            
+            test1.arrayNodes[counter]
+        
+        
+        
+        
+        
+        
+            counter += 1
+        }
+    }
---
+++ Augmented Campus/Augmented Campus/Model/FirebaseFetch.swift
@@ -0,0 +1,63 @@
+//used for fetching data from Firebase
+//and converting JSON to custom types
+
+import Firebase
+func fetchNodes() {
+	let ref: DatabaseReference!
+	ref = Database.database().reference()
+    
+    ref.child("Graph").child("Nodes").observeSingleEvent(of: .value, with: { (snapshot) in
+
+        guard let dictionary = snapshot.value as? [String: [String: Any]] else {
+            return
+        }
+       convertToNavigationLocations(from: dictionary)
+    }) { (error) in
+        print(error.localizedDescription)
+    }
+}
+
+var arrayNodes: [NavigationLocation] = [NavigationLocation(lat: 0, lng: 0, name: "a", id: -1)] //global variable that is used to store values for parsing
+//var edgeList: [string]
+    //global variables that stores edges
+func convertToNavigationLocations(from dict: [String: [String: Any]]) {
+   var counter = 0
+    for properties in dict.values {
+        
+        let id = properties["_id"] as! Int        
+        let lat = properties["latitude"] as! Double
+        let lng = properties["longitude"] as! Double
+        let dsc = properties["description"] as! String
+        let navLoc = NavigationLocation(lat: lat, lng: lng, name: dsc, id: id, indexID: counter)
+        arrayNodes.append(navLoc)
+        counter+=1
+    }
+
+}
+
+
+func fetchNodes() {
+    let ref: DatabaseReference!
+    ref = Database.database().reference()
+    
+    ref.child("Graph").child("Edges").observeSingleEvent(of: .value, with: { (snapshot) in
+        
+        guard let dictionary = snapshot.value as? [String: [String: Any]] else {
+            return
+        }
+       
+    }) { (error) in
+        print(error.localizedDescription)
+    }
+    
+}
+/*
+func convertToEdgeValues( from dict: [String: [String: Any]]) {
+    var counter = 0
+    for strings in dict.values {
+        edgeList.append(strings)
+        //Add the "stairs" functionality
+    }
+}
+ */
+
